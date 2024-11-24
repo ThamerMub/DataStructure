d1b91edf5e4fa3578a486fd8fb64dad23d5f55d4
Thamer <thamermub@hotmail.com>
2024-11-24 19:55:53 +0300

    fix some problems

--- src/main.java
+++ src/main.java
@@ -9,13 +9,14 @@ public class main {
         int StackChoice;
         // start of the menu
         System.out.println("welcome to our Data Structure project.");
-        System.out.println("please enter the number of the wanted data Structure: ");
-        System.out.println("1. LinkedList.");
-        System.out.println("2. Stack.");
-        System.out.println("3. Queue.");
-        System.out.println("4. Exit.");
 
         do {
+            System.out.println("please enter the number of the wanted data Structure: ");
+            System.out.println("1. LinkedList.");
+            System.out.println("2. Stack.");
+            System.out.println("3. Queue.");
+            System.out.println("4. Exit.");
+
             DS = input.nextInt();
             switch (DS) {
                 case 1: // LinkedList's swith-case or menu will be here
@@ -23,6 +24,7 @@ public class main {
                     break;
 
                 case 2: // Stack's swith-case or menu will be here
+
                     do {
                         System.out.println("\n Stack Menu");
                         System.out.println("1. Push Car to Stack");
@@ -67,14 +69,15 @@ public class main {
                             case 7:
                                 System.out.println("Returning to the main menu...");
                                 break;
+
                             default:
                                 System.out.println("the input is wrong, please try again.");
-                                break;
 
                         }
                     } while (StackChoice != 7);
 
                     break;
+
                 case 3:// Queue's swith-case or menu will be here
 
                     break;
@@ -85,5 +88,6 @@ public class main {
                     System.out.println("the input is wrong, please try again.");
             }
         } while (DS != 4);
+        input.close();
     }
 }
